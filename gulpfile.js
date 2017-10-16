53ea401ff1e257b2edb4f2b60336c34433616d2a
Rikazavr <risempai@gmail.com>
2017-10-16 09:53:48 +0000

    added new gulp tusks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var useref = require('gulp-useref');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    useref = require('gulp-useref'),
+    autoprefixer = require('gulp-autoprefixer'),
+    svgSprite	= require('gulp-svg-sprite'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
+
 
 
 gulp.task('browserSync', function() {
@@ -12,7 +17,6 @@ gulp.task('browserSync', function() {
   })
 })
 
-
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
@@ -22,6 +26,14 @@ gulp.task('sass', function() {
     }))
 });
 
+gulp.task('prefix', () =>
+    gulp.src('app/scss/**/*.scss')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/scss'))
+);
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
@@ -29,6 +41,12 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/js/**/*.js', browserSync.reload);
 });
 
+gulp.task('minifycss', function () {
+    gulp.src('app/css/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/scss'));
+});
 
 gulp.task('useref', function(){
   return gulp.src('app/*.html')
